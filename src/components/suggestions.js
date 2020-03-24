d25607c53f783aa1e5b1f8f4b0cec9d87b0da84e
grejppi <grejppi@users.noreply.github.com>
2020-03-24 11:29:48 +0200

    Don't use indexes as keys

--- src/components/suggestions.js
+++ src/components/suggestions.js
@@ -17,16 +17,16 @@ function Suggestions({ dispatch, state }) {
     <div className='flex flex-col-reverse w-full container'>
       {!state.attackTypeSuggestions.didInvalidate &&
         Object.keys(state.attackTypeSuggestions.suggestions).sort().map(
-          (damage, i) => {
+          damage => {
             return (
-              <div className='flex flex-col' key={i}>
+              <div className='flex flex-col' key={damage}>
                 <div>{damage}&times;</div>
                 <div className='flex flex-row flex-wrap'>
                   {state.attackTypeSuggestions.suggestions[damage].map(
-                    (type, j) => <Card
+                    type => <Card
                       text={type}
                       height={damage > 1 ? 'h-32' : 'h-24'}
-                      key={j}
+                      key={type}
                     />
                   )}
                 </div>
