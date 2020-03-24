3e8c3378eb7010ee2e38848b80505fe7531b651c
grejppi <grejppi@users.noreply.github.com>
2020-03-24 17:13:30 +0200

    Update query when state changes

--- src/index.js
+++ src/index.js
@@ -13,8 +13,21 @@ import rootReducer from './reducers'
 const param = (whomst) => (new URL(document.location)).searchParams.get(whomst)
 
 const store = createStore(rootReducer, applyMiddleware(thunk))
+
 store.dispatch(setAttacker(param('attacker') || 'magikarp'))
 store.dispatch(setOpponent(param('opponent') || 'stunfisk'))
+
+store.subscribe(() => {
+  const state = store.getState()
+
+  window.history.replaceState(
+    {},
+    '',
+    `?attacker=${state.attacker.pokemon}&opponent=${state.opponent.pokemon}`,
+  )
+
+  document.title = `${state.attacker.name} vs. ${state.opponent.name}`
+})
 
 ReactDOM.render(
   <Provider store={store}>
